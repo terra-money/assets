d22c263ffe4fdfbfcd3e51ff43c66a1fda4bc704
codehans <94654388+codehans@users.noreply.github.com>
2021-12-21 05:22:59 +0000

    Kujira: Airdrop Contract (#159)
    * Update contracts.js
    
    * Update contracts.js

--- cw20/contracts.js
+++ cw20/contracts.js
@@ -1015,6 +1015,16 @@ module.exports = {
       name: "Platform Fee Lockbox",
       icon: "https://assets.kujira.app/kuji.png",
     },
+    terra1nzyzc80sk9y6k58frdt8ye4y2qjq8myz44kg2w: {
+      protocol: "Kujira",
+      name: "Airdrops",
+      icon: "https://assets.kujira.app/kuji.png",
+    },
+    terra1l0c5kmclu5zkd5jwud3pzmv2j86puchnssnxwf: {
+      protocol: "Kujira",
+      name: "Marketing",
+      icon: "https://assets.kujira.app/kuji.png",
+    },
     terra19fjaurx28dq4wgnf9fv3qg0lwldcln3jqafzm6: {
       protocol: "Terraswap",
       name: "PLY-UST Pair",
