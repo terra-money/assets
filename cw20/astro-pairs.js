5a728b0bd149b7a3934bedf4465e3d2d81ce777b
stefanionescu <forgetthismail@protonmail.com>
2021-12-27 20:49:38 +0000

    Add phase 1 lockdrop Astroport pairs

---
+++ cw20/astro-pairs.js
@@ -0,0 +1,45 @@
+module.exports = {
+  mainnet: {
+    terra1m6ywlgn6wrjuagcmmezzz2a029gtldhey5k552: [
+      "uusd",
+      "uluna"
+    ],
+    terra1qr2k6yjjd5p2kaewqvg93ag74k6gyjr7re37fs: [
+      "uusd",
+      "terra14z56l0fp2lsf86zy3hty2z47ezkhnthtr9yq76"
+    ],
+    terra143xxfw5xf62d5m32k3t4eu9s82ccw80lcprzl9: [
+      "uusd",
+      "terra15gwkyepfc6xgca5t5zefzwy42uts8l2m4g40k6"
+    ],
+    terra1mxyp5z27xxgmv70xpqjk7jvfq54as9dfzug74m: [
+      "uusd",
+      "terra1mddcdx0ujx89f38gu7zspk2r2ffdl5enyz2u03"
+    ],
+    terra1m95udvvdame93kl6j2mk8d03kc982wqgr75jsr: [
+      "uusd",
+      "terra13xujxcrc9dqft4p9a8ls0w3j0xnzm6y2uvve8n"
+    ],
+    terra15s2wgdeqhuc4gfg7sfjyaep5cch38mwtzmwqrx: [
+      "uusd",
+      "terra1dy9kmlm4anr92e42mrkjwzyvfqwz66un00rwr5"
+    ],
+    terra134m8n2epp0n40qr08qsvvrzycn2zq4zcpmue48: [
+      "uusd",
+      "terra1kcthelkax4j9x8d3ny6sdag0qmxxynl3qtcrpy"
+    ],
+    terra1v5ct2tuhfqd0tf8z0wwengh4fg77kaczgf6gtx: [
+      "uusd",
+      "terra12897djskt9rge8dtmm86w654g7kzckkd698608"
+    ],
+    terra1zpnhtf9h5s7ze2ewlqyer83sr4043qcq64zfc4: [
+      "uusd",
+      "terra100yeqvww74h4yaejj6h733thgcafdaukjtw397"
+    ],
+    terra1j66jatn3k50hjtg2xemnjm8s7y8dws9xqa5y8w: [
+      "uluna",
+      "terra1kc87mu460fwkqte29rquh4hc20m54fxwtsx7gp"
+    ],
+  },
+  testnet: {}
+}
