a5227bb1fe6b812a3cd4badd48f5218ca10c6222
Canguilhem <clerger.a35@gmail.com>
2022-01-02 02:49:24 +0800

    feat(marketplace.js): add talis infos

--- cw721/marketplace.js
+++ cw721/marketplace.js
@@ -4,8 +4,13 @@ module.exports = [
     link: "https://randomearth.io",
   },
   {
-    "name": "KNOWHERE",
-    "link": "https://knowhere.art",
-    "icon": "https://knowhere.art/static/logos/knowhere-logo.svg"
+    name: "KNOWHERE",
+    link: "https://knowhere.art",
+    icon: "https://knowhere.art/static/logos/knowhere-logo.svg",
+  },
+  {
+    name: "Talis",
+    link: "https://talis.art",
+    icon: "https://talis.art/icons/talis/logo.svg",
   },
 ];
