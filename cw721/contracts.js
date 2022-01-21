74e091cbc4b1286ffffcfa3912ecf21ae2ed1810
Richard <ivmidable@gmail.com>
2022-01-21 00:12:46 -0500

    add heronft (#221)
    Co-authored-by: ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>

--- cw721/contracts.js
+++ cw721/contracts.js
@@ -411,6 +411,13 @@ module.exports = {
       icon: "https://cdn.luart.io/logos/luni-logo.jpg",
       homepage: "https://www.luniofficial.com",
     },
+    terra1lfr4aja5a2xpxvnrl4gyjpru0wwglu7k87jmeq: {
+        contract: "terra1lfr4aja5a2xpxvnrl4gyjpru0wwglu7k87jmeq",
+        name: "HERO",
+        symbol: "HERO",
+        icon: "https://d75aawrtvbfp1.cloudfront.net/misc/hero/logo.png",
+        homepage: "https://heronft.io/",
+    },
     terra1l8tx35lkzlgeh77nnzxgm8a2pzakw5khtg0npq: {
       contract: "terra1l8tx35lkzlgeh77nnzxgm8a2pzakw5khtg0npq",
       name: "UNDRGRND",
