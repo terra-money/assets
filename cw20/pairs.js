fc1861efc4d4b3ffa455be8e8c9bca15ed6a08b3
sunny334 <89019434+sunny334@users.noreply.github.com>
2022-02-09 13:31:47 +0500

    Added Loop and Loopr pair, updated pair.js
    Added Loop and Loopr pairs.

--- cw20/pairs.js
+++ cw20/pairs.js
@@ -228,6 +228,14 @@ module.exports = {
       "uusd",
       "terra1a7ye2splpfzyenu0yrdu8t83uzgusx2malkc7u"
     ],
+    terra10k7y9qw63tfwj7e3x4uuzru2u9kvtd4ureajhd: [
+      "uusd",
+      "terra1nef5jf6c7js9x6gkntlehgywvjlpytm7pcgkn4"
+    ],
+    terra18raj59xx32kuz66sfg82kqta6q0aslfs3m8s4r: [
+      "uusd",
+      "terra1jx4lmmke2srcvpjeereetc9hgegp4g5j0p9r2q"
+    ],
   },
   testnet: {
     terra1rfzwcdhhu502xws6r5pxw4hx8c6vms772d6vyu: [
