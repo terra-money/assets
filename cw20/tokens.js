5c57c69270cf7d919f9a5f4b8e7e41a59e82e4fd
Lotaxgizmo <85346622+Lotaxgizmo@users.noreply.github.com>
2022-02-10 05:37:02 +0100

    Add Capitrade token (#253)
    * Update tokens.js
    
    * fix: fix indentation
    
    * Update tokens.js
    
    Co-authored-by: ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>

--- cw20/tokens.js
+++ cw20/tokens.js
@@ -699,6 +699,14 @@ module.exports = {
       icon: "https://prismprotocol.app/xprism.png",
       decimals: 6
     },
+    terra13fs83g5atgjwuh7c5ydzh6n7gecel6xyhhy2t5: {
+      protocol: "CAPITRADE",
+      symbol: "CDE",
+      name: "CAPITRADE TOKEN",
+      token: "terra13fs83g5atgjwuh7c5ydzh6n7gecel6xyhhy2t5",
+      icon: "https://raw.githubusercontent.com/Lotaxgizmo/images/57f862e3c2a68708775dae5f962885f2ed309b2d/ICON%20COLOURED%404x%201.png",
+      decimals: 9
+    },
   },
   testnet: {
     terra1fdclww9fazjeu0g60h0ln2j6c50r42uwlm8jcg: {
