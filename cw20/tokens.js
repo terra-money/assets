73117540302760c9c5418e96216dcfc02393f972
hsbrg <tyagi.lakshay@gmail.com>
2022-03-11 03:53:16 +0530

    Update tokens.js
    testnet launch 10March

--- cw20/tokens.js
+++ cw20/tokens.js
@@ -680,7 +680,7 @@ module.exports = {
     terra1fdclww9fazjeu0g60h0ln2j6c50r42uwlm8jcg: {
       protocol: "FanFury",
       symbol: "FURY",
-      token: "terra1fdclww9fazjeu0g60h0ln2j6c50r42uwlm8jcg",
+      token: "terra1zjthyw8e8jayngkvg5kddccwa9v46s4w9sq2pq",
       icon: "https://crytpo11.s3.eu-central-1.amazonaws.com/Fanfury.png"
     },
     terra1v000amr8a59r88p33ec2kk9xqe047g7zzqqaf4: {
