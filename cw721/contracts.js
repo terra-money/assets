a50805f3fd5be7b0a413921c3367981fb1289d96
Leafaar <45247054+leafaar@users.noreply.github.com>
2022-03-15 23:30:48 -0300

    Rekt wolf (#339)
    * Rekt wolf
    
    * Rekt wolf

--- cw721/contracts.js
+++ cw721/contracts.js
@@ -586,6 +586,14 @@ module.exports = {
       icon: "https://terraworld.me/favicon.png",
       homepage: "https://terraworld.me/",
     },
+    terra1x8m8vju636xh7026dehq6g7ye66tn0yu4c7mq8: {
+      contract: "terra1x8m8vju636xh7026dehq6g7ye66tn0yu4c7mq8",
+      name: "Rekt Wolf",
+      symbol: "RW",
+      icon: "https://cdn.luart.io/logos/rekt-wolf-logo.jpg",
+      homepage: "https://docs.rektwolf.io/",
+      marketplace: ["https://marketplace.luart.io/collections/terra1x8m8vju636xh7026dehq6g7ye66tn0yu4c7mq8"],
+    },
   },
   testnet: {
     terra1225xrxqrdnxt8shtzrqnvzkc0rqxgt4577cyqm: {
