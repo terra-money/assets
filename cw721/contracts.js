17b44ad8d3ed8bb50a3036bad078f8586556ed8d
BornArcher8 <90064629+BornArcher8@users.noreply.github.com>
2022-04-07 13:27:28 +0530

    Add Astroport Lockdrop NFT's (#380)
    * Add Astroport Lockdrop NFT's
    
    Add Astroport Lockdrop NFT's. Source - https://twitter.com/astroport_fi/status/1511449476787019783.
    
    * Put the contract in mainnet rather than testnet.
    
    Put the Astroport Lockdrop NFT in mainnet rather than testnet.

--- cw721/contracts.js
+++ cw721/contracts.js
@@ -618,6 +618,12 @@ module.exports = {
       homepage: "https://www.sntntstudios.com/",
       marketplace: ["https://marketplace.luart.io/collections/terra1stzp2dlwceqh6k6cffv4zj64ddx28rdgpdal74"],
     },
+    terra1d0nmywxwm5hr0vz6v26qsz8q25m2v9j5adaws3: {
+      contract: "terra1d0nmywxwm5hr0vz6v26qsz8q25m2v9j5adaws3",
+      name: "Astroport Lockdrop NFT",
+      icon: "https://astroport.fi/astro_logo.png",
+      homepage: "https://astroport.fi/",
+    },
   },
   testnet: {
     terra1225xrxqrdnxt8shtzrqnvzkc0rqxgt4577cyqm: {
