f112960c3a0792af731d96d203686910ae32c51c
Helena <104296562+helenadecleyre@users.noreply.github.com>
2022-04-25 05:57:09 +0200

    add UNIQ testnet token (#416)
    Co-authored-by: Helena <helena@czarneckisoftware.pl>
    Co-authored-by: ALPAC-4 <81249838+ALPAC-4@users.noreply.github.com>

--- cw20/tokens.js
+++ cw20/tokens.js
@@ -1998,6 +1998,14 @@ module.exports = {
       icon: "https://terra.nexusprotocol.app/nATOM.svg",
       decimals: 6
     },
+    terra1n4jdmwdyx0043nf2fnjefa3a2s5727znd7dy39: {
+      protocol: "Unique Auction",
+      symbol: "UNIQ",
+      name: "Unique Auction Test Token",
+      token: "terra1n4jdmwdyx0043nf2fnjefa3a2s5727znd7dy39",
+      icon: "https://unique.auction/route/uniq.png",
+      decimals: 6
+    },
     terra1altrflnumhy7juwzzqgrctve84v8c7chgyw496: {
       protocol: "Neworld",
       symbol: "NWLD",
