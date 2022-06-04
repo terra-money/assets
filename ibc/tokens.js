7cc79ac4f91dcb3010c1492903d6abbf7db34931
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-06-04 18:38:58 +0200

    Add bridged Axelar tokens

--- ibc/tokens.js
+++ ibc/tokens.js
@@ -1,7 +1,61 @@
 module.exports = {
   mainnet: {
+    '2E435CEEEBA18CCB2719E0182BC5D142A364D6CCE9957DE6E1AC4D62127D2913': {
+      denom: "ibc/2E435CEEEBA18CCB2719E0182BC5D142A364D6CCE9957DE6E1AC4D62127D2913",
+      path: "transfer/channel-6",
+      base_denom: "frax-wei",
+      symbol: "axlFRAX",
+      name: "Axelar Frax",
+      icon: "https://assets.terra.money/icon/svg/axelar/FRAX.svg",
+      decimals: 18,
+    },
+    '6EFF21F9E65C9101370C38AA53049E4D1FF2B206A7C350B45F0ED3660E57AC75': {
+      denom: "ibc/6EFF21F9E65C9101370C38AA53049E4D1FF2B206A7C350B45F0ED3660E57AC75",
+      path: "transfer/channel-6",
+      base_denom: "uni-wei",
+      symbol: "axlUNI",
+      name: "Axelar Uniswap",
+      icon: "https://assets.terra.money/icon/svg/axelar/UNI.svg",
+      decimals: 18,
+    },
+    '1FD62537E1FBE67DF7574E0234112B4FE417B20AADC2F574026CB664EA9492C7': {
+      denom: "ibc/1FD62537E1FBE67DF7574E0234112B4FE417B20AADC2F574026CB664EA9492C7",
+      path: "transfer/channel-6",
+      base_denom: "link-wei",
+      symbol: "axlLINK",
+      name: "Axelar ChainLink",
+      icon: "https://assets.terra.money/icon/svg/axelar/LINK.svg",
+      decimals: 18,
+    },
+    '2E9CD07D7A6572A4CDAABBF0FBB89F69A9A362818132221182654819E277220A': {
+      denom: "ibc/2E9CD07D7A6572A4CDAABBF0FBB89F69A9A362818132221182654819E277220A",
+      path: "transfer/channel-6",
+      base_denom: "aave-wei",
+      symbol: "axlAAVE",
+      name: "Axelar AAVE",
+      icon: "https://assets.terra.money/icon/svg/axelar/AAVE.svg",
+      decimals: 18,
+    },
+    '14E4FD1AB72DE9BF1D6725CBA18373C406CB9A7DA17955299F3F4DC5C6131A4E': {
+      denom: "ibc/14E4FD1AB72DE9BF1D6725CBA18373C406CB9A7DA17955299F3F4DC5C6131A4E",
+      path: "transfer/channel-6",
+      base_denom: "wmatic-wei",
+      symbol: "axlMATIC",
+      name: "Axelar Matic",
+      icon: "https://assets.terra.money/icon/svg/axelar/MATIC.svg",
+      decimals: 18,
+    },
+    'E46EF5449878F6B81219163F211E7329CC0729AA99DA8A589A865F82F754ADE8': {
+      denom: "ibc/E46EF5449878F6B81219163F211E7329CC0729AA99DA8A589A865F82F754ADE8",
+      path: "transfer/channel-6",
+      base_denom: "dai-wei",
+      symbol: "axlDAI",
+      name: "Axelar DAI",
+      icon: "https://assets.terra.money/icon/svg/axelar/DAI.svg",
+      decimals: 18,
+    },
     'B090DC21658BD57698522880590CA53947B8B09355764131AA94EC75517D46A5': {
-      denom: "ibc/BC8A77AFBD872FDC32A348D3FB10CC09277C266CFE52081DE341C7EC6752E674",
+      denom: "ibc/B090DC21658BD57698522880590CA53947B8B09355764131AA94EC75517D46A5",
       path: "transfer/channel-8",
       base_denom: "ucre",
       symbol: "CRE",
@@ -14,7 +68,7 @@ module.exports = {
       base_denom: "weth-wei",
       symbol: "axlWETH",
       name: "Axelar Wrapped Ethereum",
-      icon: "https://assets.terra.money/icon/thorswap/ETH.svg",
+      icon: "https://assets.terra.money/icon/svg/axelar/ETH.svg",
       decimals: 18,
     },
     'B3504E092456BA618CC28AC671A71FB08C6CA0FD0BE7C8A5B5A3E2DD933CC9E4': {
@@ -23,7 +77,7 @@ module.exports = {
       base_denom: "uusdc",
       symbol: "axlUSDC",
       name: "Axelar USD Coin",
-      icon: "https://assets.terra.money/icon/thorswap/USDC.svg"
+      icon: "https://assets.terra.money/icon/svg/axelar/USDC.svg"
     },
     'CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF': {
       denom: "ibc/CBF67A2BCF6CAE343FDF251E510C8E18C361FC02B23430C121116E0811835DEF",
@@ -31,7 +85,7 @@ module.exports = {
       base_denom: "uusdt",
       symbol: "axlUSDT",
       name: "Axelar USD Tether",
-      icon: "https://assets.terra.money/icon/thorswap/USDT.svg"
+      icon: "https://assets.terra.money/icon/svg/axelar/USDT.svg"
     },
     '05D299885B07905B6886F554B39346EA6761246076A1120B1950049B92B922DD': {
       denom: "ibc/05D299885B07905B6886F554B39346EA6761246076A1120B1950049B92B922DD",
@@ -39,7 +93,7 @@ module.exports = {
       base_denom: "wbtc-satoshi",
       symbol: "axlWBTC",
       name: "Axelar Wrapped Bitcoin",
-      icon: "https://assets.terra.money/icon/thorswap/WBTC.svg",
+      icon: "https://assets.terra.money/icon/svg/axelar/WBTC.svg",
       decimals: 8,
     },
     '0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B': {
