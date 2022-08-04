829aff9082990c17661afff7f547a08099ddd3b3
DioAlma <32438619+DioAlma@users.noreply.github.com>
2022-08-04 17:58:58 +0100

    Update pairs.dex.js (#565)

--- cw20/pairs.dex.js
+++ cw20/pairs.dex.js
@@ -1,5 +1,20 @@
 module.exports = {
   mainnet: {
+    terra1w8246pdk9tf9d2dnu4lty5m8v3ptjltrm46vh8kd6yhr8k4ad2yskdqs6x: {
+      dex: "terraswap",
+      type: "xyk",
+      assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "uluna"],
+    },
+    terra17neqggggg4rlwh2ksk0ys8hjtxapfa4n0k5ppclw9arnh3cez93qkh4xyp: {
+      dex: "terraswap",
+      type: "xyk",
+      assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "uluna"],
+    },
+    terra1xjv2pmf26yaz3wqft7caafgckdg4eflzsw56aqhdcjw58qx0v2mqux87t8: {
+      dex: "terraswap",
+      type: "xyk",
+      assets: ["terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg", "uluna"],
+    },
     terra10xuatumr0g345umf7qtn0svvy83k0wxmdyf698yrs9lkytyd6srs0g2vut: {
       dex: "terraswap",
       type: "xyk",
