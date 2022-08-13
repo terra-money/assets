56c49776d57b1ff61606cfa3ab14290a18eb50b8
DioAlma <32438619+DioAlma@users.noreply.github.com>
2022-08-13 14:09:20 +0100

    Update pairs.dex.js

--- cw20/pairs.dex.js
+++ cw20/pairs.dex.js
@@ -1,10 +1,55 @@
 module.exports = {
-  mainnet: {
+  mainnet: {   
+    terra1gu75wek7kq8h4ee6eztmfu73nr3esl6al0qjawkhya3g57sz6jvsukpj3z: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg", "terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q"],
+    },    
+    terra1eryp83n366l4583nk8ln5qv7lvdeejzsx7vekf32e6qj33cxwguqns2vxr: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q"],
+    },    
+    terra1qg85dekl59jv723ce54s82v26rteknru5645lfm3n9eytr53570ssrz6js: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg"],
+    },    
+    terra1nfv5kddgfj2y77phtu0yumwnw5vz4m8anz2avaw4dpf2w0wlu02s9rkc56: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q"],
+    },    
+    terra1wm9dlwgtufjnjzuuee8ftqy3t9vq728vhyxv0tuqgzk7dt3fmwwsecqh8j: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg"],
+    },    
+    terra1zmv0qqangw0fp78xtzyzksmzl6weev5pm6d6y44k4z4lvszv6gqq04m2l0: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8"],
+    },
     terra1w8246pdk9tf9d2dnu4lty5m8v3ptjltrm46vh8kd6yhr8k4ad2yskdqs6x: {
-      dex: "terraswap",
+      dex: "astroport",
       type: "xyk",
       assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "uluna"],
-    },    
+    }, 
+    terra17neqggggg4rlwh2ksk0ys8hjtxapfa4n0k5ppclw9arnh3cez93qkh4xyp: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "uluna"],
+    },
+    terra1xjv2pmf26yaz3wqft7caafgckdg4eflzsw56aqhdcjw58qx0v2mqux87t8: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg", "uluna"],
+    },
+    terra10xuatumr0g345umf7qtn0svvy83k0wxmdyf698yrs9lkytyd6srs0g2vut: {
+      dex: "astroport",
+      type: "xyk",
+      assets: ["terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q", "uluna"],
+    },
     terra1gu75wek7kq8h4ee6eztmfu73nr3esl6al0qjawkhya3g57sz6jvsukpj3z: {
       dex: "terraswap",
       type: "xyk",
@@ -15,7 +60,7 @@ module.exports = {
       type: "xyk",
       assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q"],
     },    
-    terra1cmf8ytutcwrjrv08zskj9phuh46a3w3nkjax7en4hxezsrdr58lqvzy05q: {
+    terra1qg85dekl59jv723ce54s82v26rteknru5645lfm3n9eytr53570ssrz6js: {
       dex: "terraswap",
       type: "xyk",
       assets: ["terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8", "terra1qj5hs3e86qn4vm9dvtgtlkdp550r0rayk9wpay44mfw3gn3tr8nq5jw3dg"],
@@ -35,6 +80,11 @@ module.exports = {
       type: "xyk",
       assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "terra1sdglum2dt4f3fmq7jrt2phf2tegmnudc7qqqqujkpqcm9ujuxxkqakv5u8"],
     },
+    terra1w8246pdk9tf9d2dnu4lty5m8v3ptjltrm46vh8kd6yhr8k4ad2yskdqs6x: {
+      dex: "terraswap",
+      type: "xyk",
+      assets: ["terra1spkm49wd9dqkranhrks4cupecl3rtgeqqljq3qrvrrts2ev2gw6sy5vz3k", "uluna"],
+    },    
     terra17neqggggg4rlwh2ksk0ys8hjtxapfa4n0k5ppclw9arnh3cez93qkh4xyp: {
       dex: "terraswap",
       type: "xyk",
