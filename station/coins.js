31ab711c075b53c884d55813e7bdf099b433852e
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-11-10 11:09:57 +0100

    Add coins.json

---
+++ station/coins.js
@@ -0,0 +1,32 @@
+module.export = {
+    uluna: {
+        token: 'uluna',
+        symbol: 'Luna',
+        name: 'Terra Luna',
+        icon: 'https://assets.terra.money/icon/svg/Luna.svg',
+        decimals: 6,
+    },
+    uosmo: {
+        token: 'uosmo',
+        symbol: 'Osmo',
+        name: 'Osmosis',
+        icon: 'https://assets.terra.money/icon/svg/ibc/OSMO.svg',
+        decimals: 6,
+    },
+    // Luna on Osmosis
+    '785AFEC6B3741100D15E7AF01374E3C4C36F24888E96479B1C33F5C71F364EF9': {
+        token: 'uluna',
+        symbol: 'Luna',
+        name: 'Terra Luna',
+        icon: 'https://assets.terra.money/icon/svg/Luna.svg',
+        decimals: 6,
+    },
+    // Osmo on Terra
+    '0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B': {
+        token: 'uosmo',
+        symbol: 'Osmo',
+        name: 'Osmosis',
+        icon: 'https://assets.terra.money/icon/svg/ibc/OSMO.svg',
+        decimals: 6,
+    }
+}
