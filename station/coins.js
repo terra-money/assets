2f1ef31487ddb989899b54a14e426b57f44fa178
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2022-11-10 16:04:35 +0100

    fix IBC token address

--- station/coins.js
+++ station/coins.js
@@ -24,7 +24,7 @@ module.exports = {
         decimals: 6,
     },
     // Luna on Osmosis
-    '785AFEC6B3741100D15E7AF01374E3C4C36F24888E96479B1C33F5C71F364EF9': {
+    'ibc/785AFEC6B3741100D15E7AF01374E3C4C36F24888E96479B1C33F5C71F364EF9': {
         token: 'uluna',
         symbol: 'Luna',
         name: 'Terra Luna',
@@ -33,7 +33,7 @@ module.exports = {
         decimals: 6,
     },
     // Osmo on Terra
-    '0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B': {
+    'ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B': {
         token: 'uosmo',
         symbol: 'Osmo',
         name: 'Osmosis',
