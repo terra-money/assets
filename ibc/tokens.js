1150b26d1fdb9c8fcf6533c555ab137a775b4d49
rekiz <rekiz@qq.com>
2022-12-22 01:44:49 +0800

    Update tokens.js (#676)
    support OSMO ibc asset for classic

--- ibc/tokens.js
+++ ibc/tokens.js
@@ -162,6 +162,14 @@ module.exports = {
       name: "Cosmos",
       icon: "https://assets.terra.money/icon/svg/ibc/ATOM.svg",
     },
+    "0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B": {
+      denom: "ibc/0471F1C4E7AFD3F07702BEF6DC365268D64570F7C1FDC98EA6098DD6DE59817B",
+      path: "transfer/channel-1",
+      base_denom: "uosmo",
+      symbol: "OSMO",
+      name: "Osmosis",
+      icon: "https://assets.terra.money/icon/svg/ibc/OSMO.svg",
+    },
     EB2CED20AB0466F18BE49285E56B31306D4C60438A022EA995BA65D5E3CF7E09: {
       denom: "ibc/EB2CED20AB0466F18BE49285E56B31306D4C60438A022EA995BA65D5E3CF7E09",
       path: "transfer/channel-16",
