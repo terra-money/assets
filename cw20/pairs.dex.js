c8f1e982af8194b7d9d8634d2f6b0bcc5fc88157
Bjlly123 <103342421+Bjlly123@users.noreply.github.com>
2023-01-01 17:19:15 -0600

    Update pairs.dex.js
    Add Moon Dust/Luna Classic pair

--- cw20/pairs.dex.js
+++ cw20/pairs.dex.js
@@ -1565,6 +1565,11 @@ module.exports = {
       type: 'xyk',
       assets: ['uusd', 'terra1vsyyg4rs483uzd7jzw5fqqlm2q5n2yd37yylfc',],
     },
+    terra1h03vagsnr02k6dps3j0mpqszzda09s9v03rnu7: {
+      dex: 'terraswap',
+      type: 'xyk',
+      assets: ['terra1pkp0pkxp9mtlan7cg0qatu65hmkgyq2342g27g', 'uluna'],
+    },
   },
   testnet: {},
 };
