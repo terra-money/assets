4242e3869911a3317f4df067e6de8155d41683d5
Joshua Brigati <brigati.joshua@gmail.com>
2023-01-13 15:13:31 -0600

    Add uhuahua to coingecko file

--- station/coingecko.js
+++ station/coingecko.js
@@ -10,6 +10,7 @@ module.exports = {
     ugraviton: 'graviton',
     ukuji: 'kujira',
     ucre: 'crescent-network',
+    uhuahua: 'chihuahua-token',
     'factory:kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7:uusk': 'usk',
     'frax-wei': 'frax',
     'uni-wei': 'uniswap',
