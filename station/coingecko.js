a492c6decd87053e3e0133fabaa1cdf986aacc77
Alessandro Candeago <54709706+alecande11@users.noreply.github.com>
2023-01-13 22:48:26 +0100

    Add huahua coingeko id

--- station/coingecko.js
+++ station/coingecko.js
@@ -10,6 +10,8 @@ module.exports = {
     ugraviton: 'graviton',
     ukuji: 'kujira',
     ucre: 'crescent-network',
+    swth: 'switcheo',
+    uhuahua: 'chihuahua-token',
     'factory:kujira1qk00h5atutpsv900x202pxx42npjr9thg58dnqpa72f2p7m2luase444a7:uusk': 'usk',
     'frax-wei': 'frax',
     'uni-wei': 'uniswap',
