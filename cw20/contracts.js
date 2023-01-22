8c6668dc7c07fc2888fc08a65490530526bc7154
lbunproject <122697309+lbunproject@users.noreply.github.com>
2023-01-21 20:44:51 -0600

    Update contracts.js
    Added LUNC Burn Token to Terra 2

--- cw20/contracts.js
+++ cw20/contracts.js
@@ -80,6 +80,11 @@ module.exports = {
       name: "Santerra prefund",
       icon: "https://santerra.app/sant_logo.svg",
     },
+    terra1tmehj9z2yemgc8zvn6gv4c6zxd67zaw8aejl75hz5sn4tjpycpjsqrgq58: {
+      protocol: "LBUNswap",
+      name: "LUNC Burn Token",
+      icon: "https://raw.githubusercontent.com/lbunproject/LBUNswap-web-app_Terra2/main/public/images/others/LBUN.svg",
+    },
   },
   classic: {
     terra1dq03ugtd40zu9hcgdzrsq6z2z4hwhc9tqk2uy5: {
