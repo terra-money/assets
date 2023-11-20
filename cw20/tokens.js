f7f40395df5b33b19be9d6ed32c531b4bfdca3b1
Andre Amorim <andre.php6@gmail.com>
2023-11-20 16:37:35 -0300

    Added Tritium token

--- cw20/tokens.js
+++ cw20/tokens.js
@@ -2072,6 +2072,14 @@ module.exports = {
       icon: "https://i.ibb.co/jkwPpnS/zz1.png",
       decimals: 6,
     },
+    terra1g6fm3yu79gv0rc8067n2nnfpf0vks6n0wpzaf4u7w48tdrmj98zsy7uu00: {
+      protocol: "Terra Tritium",
+      symbol: "TRIT",
+      name: "Tritium Token",
+      token: "terra1g6fm3yu79gv0rc8067n2nnfpf0vks6n0wpzaf4u7w48tdrmj98zsy7uu00",
+      icon: "https://raw.githubusercontent.com/terra-tritium/assets/main/trit.png",
+      decimals: 6,
+    },
   },
   testnet: {
     terra1293l9rgqk5vxndaeqmengssg3pdch57qpepgslh7p2sg3lqd87rss8du4s: {
